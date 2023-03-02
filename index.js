c430362466433d4ac0f9d431cc8290998968d3e7
Afolabi <opakunleafolabi@gmail.com>
2023-03-02 12:36:00 +0100

    header implemented

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end('Welcome Afolabi');
+    const pathName = req.url;
+    if(pathName === '/overview' || pathName === '/') {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+        });
+        res.end('<h2>Welcome to overview</h2>');
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/html'
+        });
+        res.end('<h4>Page not found</h4>');
+    }
 });
 
-server.listen(3000, '127.0.0.1',() => console.log('Up and running'));
+server.listen(3000, '127.0.0.1', () => console.log('Up and running'));
