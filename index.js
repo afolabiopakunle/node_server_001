9e0a5004e94b762bbce17ff6f5526eb1aeb7db08
Afolabi <opakunleafolabi@gmail.com>
2023-03-08 00:30:40 +0100

    code refactored into modules

--- index.js
+++ index.js
@@ -2,28 +2,13 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const tempFoodCard = fs.readFileSync(`${__dirname}/templates/template_food.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template_product.html`, 'utf-8');
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{{PRODUCT_NAME}}/g, product.productName);
-    output = output.replace(/{{IMAGE}}/g, product.image);
-    output = output.replace(/{{PRICE}}/g, product.price);
-    output = output.replace(/{{FROM}}/g, product.from);
-    output = output.replace(/{{NUTRIENTS}}/g, product.nutrients);
-    output = output.replace(/{{QUANTITY}}/g, product.quantity);
-    output = output.replace(/{{DESCRIPTION}}/g, product.description);
-    output = output.replace(/{{ID}}/g, product.id);
-    output = output.replace(/{{QUANTITY}}/g, product.quantity);
-
-    if(!product.organic) {
-        output = output.replace(/{{NOT_ORGANIC}}/g,'not-organic');
-    }
-    return output
-}
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
